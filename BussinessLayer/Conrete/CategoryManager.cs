1e916eaa71adae6b40d899fbfc1d09e1ca19b562
ozi <oguzhancevik.developergmail.com>
2022-02-03 21:37:26 +0300

    Manager and service write in the bussiness layer.

---
+++ BussinessLayer/Conrete/CategoryManager.cs
@@ -0,0 +1,38 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Conrete
+{
+    public class CategoryManager : ICategoryDal
+    {
+        public void AddCategory(Category category)
+        {
+            
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> ListAllCategory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
