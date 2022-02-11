d077bac52202d364621b9a9ab9413b3c040d56d2
ozi <oguzhancevik.developergmail.com>
2022-02-11 17:35:00 +0300

    List of Blogs

--- EntityLayer/Concrete/Blog.cs
+++ EntityLayer/Concrete/Blog.cs
@@ -10,13 +10,13 @@ namespace EntityLayer.Concrete
     public class Blog
     {
         [Key]
-        public int AboutID { get; set; }
-        public string AboutDetail1 { get; set; }
-        public string AboutDetail2 { get; set; }
-        public string AboutImage1 { get; set; }
-        public string AboutImage2 { get; set; }
-        public string AboutMapLocation { get; set; }
-        public bool AboutStatus { get; set; }
+        public int BlogID { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogContent { get; set; }
+        public string BlogThumbnai { get; set; }
+        public string BlogImage { get; set; }
+        public string BlogCreateDate { get; set; }
+        public bool BlogStatus { get; set; }
         public int CategoryID { get; set; }
         public Category Category { get; set; }
         public List<Comment> Comments { get; set; }
